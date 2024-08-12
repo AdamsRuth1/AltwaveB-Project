1964442ad214cc6a84e036fd90f318a7b5e74ba5
Adams Ruth <adamsruth4080@gmail.com>
2024-08-11 18:19:38 -0700

    Update CreatePlan.jsx

--- src/components/dashboard/target-Savings/CreatePlan.jsx
+++ src/components/dashboard/target-Savings/CreatePlan.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import Coin from "../../../assets/images/151055041_b058e357-f1d4-48f6-8977-25b8bead6dcc-removebg-preview 1.png";
 import Coin1 from "../../../assets/images/151055041_b058e357-f1d4-48f6-8977-25b8bead6dcc-removebg-preview 2.png";
 import Image from "../../../assets/images/2151266604 (1) 1.png";
-import Ongoing from "./Ongoing";
-import Completed from "./Completed";
-import Tuition from "./PlanCreate";
+import Ongoing from "./ongoing";
+import Completed from "./completed";
+import Tuition from "./planCreate";
 import Terminated from "./terminated";
 import Button from "./Button";
 
